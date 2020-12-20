eacc448e552b5cbfa51f0fcf3c990d00614b77ea
Samuilad <68762131+Samuilad@users.noreply.github.com>
2020-12-19 18:03:55 -0600

    Added SMS sending capability

--- app/src/main/java/Winter/Project/Application/MainActivity.java
+++ app/src/main/java/Winter/Project/Application/MainActivity.java
@@ -1,19 +1,54 @@
 package Winter.Project.Application;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.telephony.SmsManager;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 private Button button;
+public final int MY_PERMISSIONS_REQUESTS = 3;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		//Checks permissions
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED
+				|| ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+				||ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+				|| ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(this)
+						.setMessage("These Permissions are required to send your contacts valuable information of potential situation")
+						.setPositiveButton("Okay", (dialog, which) -> ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA},
+								MY_PERMISSIONS_REQUESTS))
+						.setNegativeButton("No", (dialog, which) -> dialog.dismiss());
+				builder.show();
+			}
+		}
+
+		/*Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				sendSMS();
+			}
+		}, 5000); */
+
 		setContentView(R.layout.activity_main);
 		//Buttons
 		button = findViewById(R.id.sendTo);
@@ -21,17 +56,57 @@ private Button button;
 		button = findViewById(R.id.test2);
 		button.setOnClickListener(this);
 	}
+	/*@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		switch (requestCode) {
+			case MY_PERMISSIONS_REQUESTS: {
+				// If request is cancelled, the result arrays are empty.
+				if (grantResults.length > 0
+						&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// this if loop will give grant status for Contacts
+				} else if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+					// grant status for Location}
+				}
+			}
+		}
+	}*/
+
+	/**
+	 * sends a
+	 * @param phoneNumber message receiver.
+	 * @param message message.
+	 */
+	public void sendSMS(String phoneNumber, String message) {
+		try {
+			SmsManager.getDefault().sendTextMessage(phoneNumber, null, message, null, null);
+		} catch (Exception e) {
+			AlertDialog.Builder alertDialogBuilder = new
+					AlertDialog.Builder(this);
+			AlertDialog dialog = alertDialogBuilder.create();
+
+
+			dialog.setMessage(e.getMessage());
+
+
+			dialog.show();
+
+
+		}
+	}
+	/**
+	 * sends and SMS message.
+	 * @param view idk.
+	 */
 	@Override
 	public void onClick(View view) {
-		switch (view.getId()) {
-			case R.id.test2:
-				System.out.println("clicked");
+		if (view.getId() == R.id.test2) {
+			sendSMS("6304646531", "Sent from app");
 		}
-		//starts up person activity
-		switch (view.getId()) {
-			case R.id.sendTo:
-				Intent intent = new Intent(this, PeopleActivity.class);
-				startActivity(intent);
+		//starts up person activity.
+		if (view.getId() == R.id.sendTo) {
+			Intent intent = new Intent(this, PeopleActivity.class);
+			startActivity(intent);
 		}
 
 	}
--- app/src/main/java/Winter/Project/Application/PeopleActivity.java
+++ app/src/main/java/Winter/Project/Application/PeopleActivity.java
@@ -1,37 +1,85 @@
 package Winter.Project.Application;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.telephony.SmsManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static java.lang.Integer.parseInt;
 
 public class PeopleActivity extends AppCompatActivity implements View.OnClickListener {
-	private HashMap<String, Integer> contacts = new HashMap<>();
-
+	private HashMap<String, String> contacts = new HashMap<>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_people);
-		LinearLayout person = (LinearLayout) findViewById(R.id.person_info);
+		LinearLayout person = findViewById(R.id.person_info);
+		for (TextView contact : getContactList()) {
+			contact.setVisibility(View.GONE);
+		}
+		person.setVisibility(View.GONE);
 		Button btn = findViewById(R.id.Contact);
 		btn.setOnClickListener(this);
 		btn = findViewById(R.id.Done);
 		btn.setOnClickListener(this);
-
-		person.setVisibility(View.GONE);
-
 	}
+
+	/**
+	 * @return returns list of Contacts.
+	 */
+	public ArrayList<TextView> getContactList() {
+		TextView contact1 = findViewById(R.id.Contact1);
+		TextView contact2 = findViewById(R.id.Contact2);
+		TextView contact3 = findViewById(R.id.Contact3);
+		TextView contact4 = findViewById(R.id.Contact4);
+		TextView contact5 = findViewById(R.id.Contact5);
+		TextView contact6 = findViewById(R.id.Contact6);
+		TextView contact7 = findViewById(R.id.Contact7);
+		TextView contact8 = findViewById(R.id.Contact8);
+		return (ArrayList<TextView>) new ArrayList() {{
+			add(contact1);
+			add(contact2);
+			add(contact3);
+			add(contact4);
+			add(contact5);
+			add(contact6);
+			add(contact7);
+			add(contact8);
+		}};
+	}
+
+	/**
+	 * Retrieves requested Contact.
+	 * @param number identifies which contact.
+	 * @return returns contact.
+	 */
+	public TextView getContact(int number) {
+		int i = 1;
+		for (TextView contact : getContactList()) {
+			if (i == number) {
+				return contact;
+			} else {
+				i++;
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+	/**
+	 * opens up contact info window.
+	 */
 	public void contact() {
-		LinearLayout person = (LinearLayout) findViewById(R.id.person_info);
+		LinearLayout person = findViewById(R.id.person_info);
 
 		if (person.getVisibility() == View.VISIBLE) {
 			person.setVisibility(View.GONE);
@@ -40,32 +88,49 @@ public class PeopleActivity extends AppCompatActivity implements View.OnClickLis
 			person.setVisibility(View.VISIBLE);
 		}
 	}
+
+	/**
+	 * clears text in editText objects for name and phone number.
+	 */
+	public void clearEditText() {
+		EditText contactName = findViewById(R.id.editName);
+		EditText contactPhone = findViewById(R.id.editPhone);
+		contactName.setText("");
+		contactPhone.setText("");
+	}
+	/**
+	 * Sets contact entry window to View.GONE.
+	 */
 	public void done() {
 		EditText contactName = findViewById(R.id.editName);
 		EditText contactPhone = findViewById(R.id.editPhone);
-		LinearLayout person = (LinearLayout) findViewById(R.id.person_info);
+		LinearLayout person = findViewById(R.id.person_info);
+		person.setVisibility(View.GONE);
 
-		if (contactName.length() == 0 || contactPhone.length() == 0) {
-			Toast.makeText(PeopleActivity.this, "Field(s) Empty", Toast.LENGTH_SHORT).show();
+		if (contactName.length() == 0 || contactPhone.length() != 10) {
+			Toast.makeText(PeopleActivity.this, "Field(s) Empty or Invalid Phone #", Toast.LENGTH_SHORT).show();
 		} else {
 			String name = contactName.getText().toString();
-			Integer phoneNumber = parseInt(contactPhone.getText().toString());
+			String phoneNumber = contactPhone.getText().toString();
 			contacts.put(name, phoneNumber);
+			for (TextView contact : getContactList()) {
+				if (contact.getText().toString().length() == 0) {
+					contact.setText(name);
+					contact.setVisibility(View.VISIBLE);
+					clearEditText();
+					break;
+				}
+			}
 		}
-
-		person.setVisibility(View.GONE);
 	}
 	@Override
 	public void onClick(View view) {
-		switch (view.getId()) {
-			case R.id.Contact:
-				System.out.println("here1");
-				contact();
+		if (view.getId() == R.id.Contact) {
+			contact();
+			clearEditText();
 		}
-		//gets rid of contact entry windows and enters data
-		switch (view.getId()) {
-			case R.id.Done:
-				done();
+		if (view.getId() == R.id.Done) {
+			done();
 		}
 	}
 }
